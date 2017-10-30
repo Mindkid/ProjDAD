5bd761456f83f4f78b639124b1bfcdbc29c0ad10
Mindkid <prm.4555@gmail.com>
2017-10-30 10:35:39 +0000

    Done the pear 2 pear Server. Not Testedgit add .!

---
+++ pacman/pacman/ChatRoom.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace pacman
+{
+    class ChatRoom : MarshalByRefObject
+    {
+        private List<Message> conversation;
+        private List<ChatRoom> clientsChatRoom;
+        private Form1 conversationForm;
+
+        public ChatRoom(PacmanServer server, Form1 form)
+        {
+            conversation = new List<Message>();
+            this.clientsChatRoom = server.getClientsChatRoom();
+        }
+
+        public void sendMessage(String message)
+        {
+            Thread thread = new Thread(() => broadCastMessage(message));
+            thread.Start();
+        }
+
+        public void receiveMessage(Message message)
+        {
+            conversation.Add(message);
+            conversation.Sort();
+            updateClientConversation(conversation);
+        }
+
+        private void broadCastMessage(String stringMessage)
+        {
+            Message message = new Message(stringMessage, conversation.Count + 1);
+
+            foreach (ChatRoom chat in clientsChatRoom)
+                chat.receiveMessage(message);
+        }
+        
+        private void updateClientConversation(List<Message> conversation)
+        {
+            conversationForm.Invoke(conversationForm.refreshConversation, conversation)
+        }
+
+    }
+}
---
+++ pacman/pacman/Message.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pacman
+{
+    [Serializable]
+    class Message 
+    {
+        private String message;
+        private int message_ID;
+        private DateTime dateTime;
+
+        public Message(String message, int message_ID)
+        {
+            this.message = message;
+            this.message_ID = message_ID;
+            this.dateTime = new DateTime();
+        }
+
+        public String getMessage()
+        {
+            return message;
+        }
+
+        public int getMessageID()
+        {
+            return message_ID;
+        }
+
+        public DateTime GetDateTime()
+        {
+            return dateTime;
+        }
+    }
+}
