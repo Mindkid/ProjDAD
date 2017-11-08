923f721238288254cbbe82b7561b4ab2f81de7c2
Mindkid <prm.4555@gmail.com>
2017-11-08 17:53:15 +0000

    started the PCS

--- pacman/ConnectorLibrary/ConnectionLibrary.cs
+++ pacman/ConnectorLibrary/ConnectionLibrary.cs
@@ -5,11 +5,20 @@ namespace ConnectorLibrary
     public abstract class ConnectionLibrary
     {
         public static int SERVER_PORT = 8086;
-        public static string SERVER_NAME = "Server";
+        public static String SERVER_NAME = "Server";
+
+        public static int PCS_PORT = 11000;
+        public static String PCS_NAME = "PCS";
 
         public static String buildServerURL()
         {
             return "tcp://localhost:" + SERVER_PORT + "/" + SERVER_NAME;
         }
+
+        public static String buildPCSURL()
+        {
+            return "tcp://localhost:" + PCS_PORT + "/" + PCS_NAME;
+        }
+
     }
 }
---
+++ pacman/PCS/Program.cs
@@ -0,0 +1,31 @@
+﻿using ConnectorLibrary;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryServerFormatterSinkProvider provider = new BinaryServerFormatterSinkProvider();
+            provider.TypeFilterLevel = TypeFilterLevel.Full;
+            IDictionary props = new Hashtable();
+            props["port"] = ConnectionLibrary.SERVER_PORT;
+            TcpChannel channel = new TcpChannel(props, null, provider);
+            ChannelServices.RegisterChannel(channel, false);
+
+            PCS pcs = new PCS();
+            RemotingServices.Marshal(pcs, ConnectionLibrary.SERVER_NAME, typeof(PCS));
+
+        }
+    }
+}
