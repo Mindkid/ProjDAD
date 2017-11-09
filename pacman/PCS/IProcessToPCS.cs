7d831bd331db934344518b278077c3e6e7e665d1
Mindkid <prm.4555@gmail.com>
2017-11-09 10:01:16 +0000

    PCS all done. Just need to implement IProcessToPCS.

---
+++ pacman/PCS/IProcessToPCS.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PCS
+{
+    public interface IProcessToPCS
+    { 
+        void freezeProcess();
+        
+        void unFreezeProcess();
+
+        /*
+         * Request a snapshot, the token it's represented
+         * with a random int for more detail see:
+         * https://en.wikipedia.org/wiki/Chandy-Lamport_algorithm
+         */
+        String takeSnapshot(int randomToken);
+
+        /*
+         * This process injects a delay in the channel
+         * between the target process and the destination 
+         */ 
+        void injectDelay(String destinationProcess, long delayTime);
+
+    }
+}
--- pacman/PCS/PCS.cs
+++ pacman/PCS/PCS.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Sockets;
 
 namespace PCS
 {
     public class PCS : MarshalByRefObject
     {
         private Dictionary<String, int> processIDs;
+        private Dictionary<String, IProcessToPCS> processes;
+
+        private List<int> randomTokens;
+        /*
+         * This are the paths to the given EXE files
+         */ 
         private static String clientPath = "..\\..\\..\\..\\pacman\\pacman\\bin\\Release\\pacman.exe";
         private static String serverPath = "..\\..\\..\\..\\pacman\\PacmanServer\\bin\\Release\\PacmanServer.exe";
 
@@ -17,6 +21,8 @@ namespace PCS
         public PCS()
         {
             processIDs = new Dictionary<string, int>();
+            processes = new Dictionary<string, IProcessToPCS>();
+            randomTokens = new List<int>();
         }
 
         /*
@@ -41,7 +47,10 @@ namespace PCS
             Console.WriteLine("Server Node was launched..");
         }
 
-
+        /*
+         * This process launchs a process or a client or a server
+         * and them connects the PCS with the gicen client or server
+         */ 
         private void launchProcess(String processID, String url, String path, String arguments)
         {
             ProcessStartInfo startInfo = new ProcessStartInfo(path);
@@ -49,7 +58,12 @@ namespace PCS
             startInfo.Arguments = port + " " + arguments;
 
             processIDs.Add(processID, Process.Start(startInfo).Id);
+
+            IProcessToPCS process = (IProcessToPCS)Activator.GetObject(typeof(IProcessToPCS), url);
+
+            processes.Add(processID, process);
         }
+
         /*
          * This method kills a process
          */
@@ -64,6 +78,68 @@ namespace PCS
             {
                 Console.WriteLine(processID + ": could not be killed...");
             }
+        }
+
+        public void freezeProcess(String processID)
+        {
+            try
+            {
+                processes[processID].freezeProcess();
+            }
+            catch(SocketException)
+            {
+                Console.WriteLine("Couldn't freeze process: " + processID);
+                //DO NOTHING OR REDEFINE 
+            }
+        }
+
+        public void unFreezeProcess(String processID)
+        {
+            try
+            {
+                processes[processID].unFreezeProcess();
+            }
+            catch(SocketException)
+            {
+                Console.WriteLine("Couldn't unfreeze process: " + processID);
+                //DO NOTHING OR REDEFINE 
+            }
+        }
+
+        public String globalStatus()
+        {
+            String status = "";
+            foreach (String s in processes.Keys)
+                status += localState(s);
+
+            return status;
+        }
+
+        public String localState(String processID)
+        {
+            String state = "Process: " + processID + "\r\n";
+            try
+            {
+                state += processes[processID].takeSnapshot(generateRandomUniqueToken());
+                state += "\r\n";
+            }
+            catch(SocketException)
+            {
+                state += "----PROCESS HAS CRASHED---- \r\n";
+            }
+
+            return state;
+        }
+
+        private int generateRandomUniqueToken()
+        {
+            Random generator = new Random();
+            int randomToken = generator.Next();
+            while (randomTokens.Contains(randomToken))
+                randomToken = generator.Next();
+            randomTokens.Add(randomToken);
+  
+            return randomToken;
         }
 
     }
