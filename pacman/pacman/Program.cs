8f0dfba5a6ac80d8b842e3ddcd124b83ff7fd847
RuiMDO <RuiMDO@gmail.com>
2017-12-06 17:55:42 +0000

    Client read from file.

--- pacman/pacman/Program.cs
+++ pacman/pacman/Program.cs
@@ -8,7 +8,7 @@ using System.Runtime.Remoting.Channels.Tcp;
 using System.Runtime.Serialization.Formatters;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace pacman {
     static class Program {
@@ -26,7 +26,30 @@ namespace pacman {
 
             String port = args[0].Split(':')[1].Split('/')[0];
             props["port"] = int.Parse(port);
+            String file = args[0].Split(' ').Last();
+            List<string> plays= new List<string>();
+            if (file.Contains(':'))
+            {
+                file = null;
+            }
+            if(file != null)
+            {
+                Excel.Application xlApp = new Excel.Application();
+                Excel.Workbook xlWorkbook = xlApp.Workbooks.Open("@"+file);
+                Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+                Excel.Range xlRange = xlWorksheet.UsedRange;
 
+                int rowCount = xlRange.Rows.Count;
+                int colCount = xlRange.Columns.Count;
+
+                for (int i = 1; i <= rowCount; i++)
+                {
+                    for (int j = 1; j <= colCount; j++)
+                    {
+                        plays.Add(xlRange.Cells[i, j].Value2.ToString().Split(',')[1]);
+                    }
+                }
+            }
             TcpChannel channel = new TcpChannel(props, null, provider);
             ChannelServices.RegisterChannel(channel, false);
             List<IPacmanServer> servers = new List<IPacmanServer>();
